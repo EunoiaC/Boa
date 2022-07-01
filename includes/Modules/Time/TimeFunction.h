3df6bd712ef7f311a1463ebc8c7c4b49845c4f98
AadiYadav <68039511+Cyber-cp@users.noreply.github.com>
2022-06-30 17:38:50 -0700

    fixe timefunc

--- includes/Modules/Time/TimeFunction.h
+++ includes/Modules/Time/TimeFunction.h
@@ -17,7 +17,7 @@ public:
     vector<BaseValue *> args;
     map<string, BaseValue *> defaultArgs;
     TimeFunction<T>(string name, vector<string> argNames, map<string, BaseValue *> defaultArgs, string fName, string fTxt);
-    RuntimeResult * execute(vector<BaseValue*> args) override;
+    RuntimeResult * execute(vector<BaseValue*> args, map<string, BaseValue *> kwargs) override;
     TimeFunction<int> *copy() override;
 
     RuntimeResult * execute_waitMs(Context * execCtx);
